bb2e47bc5936a4adbfc9b69cc4bcea48c58b84c8
IanL583 <133632144+IanL583@users.noreply.github.com>
2025-01-29 07:49:38 +1100

    Time: 41 ms (32.22%), Space: 17.6 MB (70.09%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,17 +1,8 @@
-class Solution(object):
-    def mergeAlternately(self, word1, word2):
-        """
-        :type word1: str
-        :type word2: str
-        :rtype: str
-        """
-        newString = ""
-        count = 0
-        while (count < len(word1) or count < len(word2)):
-            if count < len(word1):
-                newString += word1[count] 
-            if count < len(word2):
-                newString += word2[count]
-            # need to increment the loop if it is not a for loop
-            count += 1
-        return newString
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        result = ''
+        smaller_string = min(len(word1), len(word2))
+        for letter in range(smaller_string):
+            result += word1[letter] + word2[letter]
+        result += word1[smaller_string:] + word2[smaller_string:]
+        return result
