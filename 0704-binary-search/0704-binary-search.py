e9465f19c061cd471f96c319f83c39910ab6c608
IanL583 <133632144+IanL583@users.noreply.github.com>
2025-09-03 19:07:05 +1000

    Time: 0 ms (100%), Space: 18.5 MB (94.18%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,15 +1,14 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # intialise 2 pointers
-        left, right = 0, len(nums) - 1
-        # loop through the array and split array accordingly
-        while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] > target:
-                right = mid - 1
-            elif nums[mid] < target:
-                left = mid + 1
-            else:
+        start = 0
+        end = len(nums) - 1
+
+        while start <= end:
+            mid = (start + end) // 2
+            if nums[mid] == target:
                 return mid
-        # if nothing found
+            if target > nums[mid]:
+                start = mid + 1
+            if target < nums[mid]:
+                end = mid - 1
         return -1
