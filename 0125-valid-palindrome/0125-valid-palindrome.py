609e87305255cfee3adfab0cafb980ca2c78f1ab
IanL583 <133632144+IanL583@users.noreply.github.com>
2025-09-06 16:28:29 +1000

    Time: 6 ms (85.35%), Space: 18.3 MB (40.62%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,11 +1,11 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        # two pointer approach 
         clean_s = ''.join(filter(str.isalnum, s)).lower()
-        end = len(clean_s) - 1
-        for i in range(end):
-            if clean_s[i] == clean_s[end]:
-                end -= 1
-            else:
+        left, right = 0, len(clean_s) - 1
+        while left < right:
+            if clean_s[left] != clean_s[right]:
                 return False
+            left += 1
+            right -= 1
         return True
-
