642d63939d9d09f0ce21e7bd602889be61348298
IanL583 <133632144+IanL583@users.noreply.github.com>
2025-09-07 04:49:15 +1000

    Time: 11 ms (14.31%), Space: 17.8 MB (39.93%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,19 +1,18 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         roman_numerals = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000,
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
         }
         result = 0
         previous = 0
         for symbol in s:
             value = roman_numerals[symbol]
-            # need to subtract the previous value twice because it was already added once
             if value > previous:
                 result += value - 2 * previous
             else:
